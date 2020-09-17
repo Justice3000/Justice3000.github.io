1b7a51d0cdd612914d7aa6292916ed593a887533
Justice3000 <dani.indreika@gmail.com>
2020-09-17 22:39:05 +0200

    masses of changes

--- js.js
+++ js.js
@@ -1,23 +1,45 @@
-mybutton = document.getElementById("upBtn");
+upBtn = document.getElementById("upBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+  scrollbar();
+
+}
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    upBtn.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    upBtn.style.display = "none";
   }
 }
+
+
+
+function scrollbar(){
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("indicator").style.width = scrolled + "%";
+}
+
+
+
+let secret = document.getElementById('secret');
+secret.addEventListener('click', function(){
+  let audio = new Audio('oth/drone4.mp3');
+  audio.play();
+
+  document.body.style.backdropFilter = 'blur(0px) grayscale(0%)';
+
+
+
+});
+
 
 // When the user clicks on the button, scroll to the top of the document
 function goUp() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-function playMe(){
-  let audio = document.getElementById('audio');
-  audio.play();
 }
