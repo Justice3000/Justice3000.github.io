583cc3cc626db9c9a40a58c036a676ab6b7c2ba5
Justice3000 <dani.indreika@gmail.com>
2020-09-18 03:17:03 +0200

    autosave

--- js.js
+++ js.js
@@ -1,3 +1,8 @@
+
+checkStorage();
+
+
+
 upBtn = document.getElementById("upBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -27,15 +32,74 @@ function scrollbar(){
 
 
 let secret = document.getElementById('secret');
-secret.addEventListener('click', function(){
+  secret.addEventListener('click', secretF);
+
+  function secretF(){
   let audio = new Audio('oth/drone4.mp3');
   audio.play();
 
+  
   document.body.style.backdropFilter = 'blur(0px) grayscale(0%)';
+  
+  let card = document.getElementsByClassName('card-img');
+  for (let i = 0, max = card.length; i < max; i++){
+    card[i].style.opacity = "0.8"; 
+    card[i].style.filter = "invert(100%)"; 
+  }
+
+    let cardText = document.getElementsByClassName('card-text');
+    for (let i = 0, max = cardText.length; i < max; i++){
+      cardText[i].style.filter = "blur(0px)";}
+
+      let Addition = document.getElementsByClassName('card-text')[0];
+      Addition.innerHTML = ('I am here to tell you that you did find the secret!');
+      
+      localStorage.clear();
+      localStorage.setItem('DaniSecret',"found");
+      
+}
+
+
+function bypass(){
+  let audio = new Audio('oth/drone4.mp3');
+  audio.play();
+  document.body.style.backdropFilter = 'blur(0px) grayscale(0%)';
+  let card = document.getElementsByClassName('card-img');
+  for (let i = 0, max = card.length; i < max; i++){
+    card[i].style.opacity = "0.8"; 
+    card[i].style.filter = "invert(100%)"; 
+  }
+
+    let cardText = document.getElementsByClassName('card-text');
+    for (let i = 0, max = cardText.length; i < max; i++){
+      cardText[i].style.filter = "blur(0px)";}
+
+      let Addition = document.getElementsByClassName('card-text')[0];
+      Addition.innerHTML = ('You skipped the secret');
+
+      let skip = document.getElementById('skipSecret');
+      skip.parentNode.removeChild(skip);
+      localStorage.setItem('DaniSecret',"skipped");
+}
 
 
 
-});
+function checkStorage(){
+  if (localStorage.getItem('DaniSecret') == "skipped"){
+    let Addition = document.getElementsByClassName('card-text')[0];
+    Addition.innerHTML = ('You skipped the secret');
+
+    let skip = document.getElementById('skipSecret');
+    skip.parentNode.removeChild(skip);
+    
+  } else if (localStorage.getItem('DaniSecret') == "found"){
+    let skip = document.getElementById('skipSecret');
+    skip.parentNode.removeChild(skip);
+    let Addition = document.getElementsByClassName('card-text')[0];
+    Addition.innerHTML = 'But the interweb names me as Justice3000. ';
+
+  }
+}
 
 
 // When the user clicks on the button, scroll to the top of the document
@@ -43,3 +107,6 @@ function goUp() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
